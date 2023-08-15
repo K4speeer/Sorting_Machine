b43143c7d9d6f27b348d3ccb84fd1d3b748cd46f
K4speeer <dri.taha24@gmail.com>
2023-08-16 01:51:47 +0300

    Updating classes

--- sorting_machine/sm_objects.py
+++ sorting_machine/sm_objects.py
@@ -34,16 +34,18 @@ class Cassette:
         self.board = board
         self.step_pin = step_pin
         self.dir_pin = dir_pin
+        self.direction_value = 1 # 1 Clockwise , 0 CounterClockwise
         self.feed_step = 36
         self.drop_step = 60
         self.max_items = 10
         self.items = []
         self.pulse_delay = 0.05
         self.steps_per_revolution = 200 # 1 motor step = 1.8 degree (NEMA-17 stepper motor)
-        
+        self.initial_pos = 0
+        self.current_pos = 0
         
     def rotate(self, angle):
-        self.board.digital_write(self.dir_pin, 1)
+        self.board.digital_write(self.dir_pin, self.direction_value)
         steps_to_rotate = int(self.steps_per_revolution * angle / 360)
         
         for _ in range(steps_to_rotate):
@@ -51,6 +53,19 @@ class Cassette:
             sleep(self.pulse_delay)
             self.board.digital_write(self.step_pin, 0)
             sleep(self.pulse_delay)
+        
+        self.current_pos += angle
+        
+    def origin(self):
+        position = self.current_pos % 360
+        if position >= 180:
+            angle = 360 - position
+            self.rotate(angle)
+        else:
+            self.direction_value = 0
+            self.rotate(position)
+            sleep(4)
+            self.direction_value = 1
             
     def add_item(self, item):
         self.items.append(item)
