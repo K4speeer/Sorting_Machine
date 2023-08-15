1cd77a84a6e0aa611b0b56b93685f2a07c90c867
K4speeer <dri.taha24@gmail.com>
2023-08-15 23:26:35 +0300

    Adding __init__.py file for hardware initialization

---
+++ sorting_machine/__init__.py
@@ -0,0 +1,8 @@
+# Importing the color prediction function
+from .color_cnn import color_model_predict
+# Importing the size prediction function
+from .size_cnn import size_model_predict
+# Import Pymata4 library for serial communication 
+from pymata4 import pymata4
+ 
+from time import sleep
