371560889ccfd7653b75b8951bb31c1e4eb51d63
K4speeer <dri.taha24@gmail.com>
2023-08-21 23:45:12 +0300

    Moving database model into seperate folder

---
+++ website/models.py
@@ -0,0 +1,26 @@
+from . import db
+
+class sessions(db.Model):
+        _id = db.Column('id', db.Integer(), primary_key=True )
+        dateTime = db.Column('SessionDate', db.DateTime(), nullable=False)
+        start_time = db.Column('sessionStartTime', db.String(10), nullable=False)
+        end_time = db.Column('sessionEndTime', db.String(10), nullable=False)
+        total_objects = db.Column('ObjectsQuantity', db.Integer(), nullable=False)
+        gate1_params = db.Column('gate1Params', db.String(25))
+        gate2_params = db.Column('gate2Params', db.String(25))
+        gate3_params = db.Column('gate3Params', db.String(25))
+        gate1_objects = db.Column('gate1Objects', db.Integer())
+        gate2_objects = db.Column('gate2Objects', db.Integer())
+        gate3_objects = db.Column('gate3Objects', db.Integer())
+        # Model initialization function 
+        def __init__(self, dateTime, total_objects,start_time, end_time, gate1_params, gate2_params, gate3_params, gate1_objects, gate2_objects, gate3_objects):
+            self.dateTime = dateTime
+            self.start_time = start_time
+            self.end_time = end_time
+            self.total_objects = total_objects
+            self.gate1_params = gate1_params
+            self.gate2_params = gate2_params
+            self.gate3_params = gate3_params
+            self.gate1_objects = gate1_objects
+            self.gate2_objects = gate2_objects
+            self.gate3_objects = gate3_objects
