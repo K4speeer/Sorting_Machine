42b113d380615a6f41cb59c93956614036167f4b
K4speeer <dri.taha24@gmail.com>
2023-08-23 20:31:14 +0300

    Added print statements to track execution

--- sorting_machine/__init__.py
+++ sorting_machine/__init__.py
@@ -13,21 +13,37 @@ servo_configs = {6:[115, 40, 60],
                  9:[90, 0, 180],
                  10:[90, 180, 240]}
 
-def init_machine(ir_pin, step_pin, dir_pin, servo_configs):
+def init_board():
+    """Pymata4() board initialization for Serial Communicateion
+
+    Returns:
+        Pymata4(): Arduino board instance as Pymata4() object
+    """
+    print("Arduino board as Pymata4() instance initialized")
+    return pymata4.Pymata4()
+
+def init_machine(board, ir_pin, step_pin, dir_pin, servo_configs):
     # Initialize the machine object and set pins to use.
-    board = pymata4.Pymata4()
+
     cassette = Cassette(board, step_pin, dir_pin)
+    print("Casseette instance initialized with parameters: "/
+          f"Step pin of driver = {step_pin}"/
+          f"Direction pin of driver = {dir_pin}")
     gates_list = []
     for key, value in servo_configs:
-        pin = key 
+        pin = key
         close_pos = value[0]
         open_pos = value[1]
         pos = value[2]
         gate = Gate(board, pin, close_pos, open_pos, pos)
+        print(f"Servo connected to pin: {key} initialized successfully with parameters: "/
+              f"Close position: {close_pos}"/
+              f"Open position: {open_pos}"/
+              f"Position depending on cassette origin: {pos}")
         gate.close()
         gates_list.append(gate)
     
     sm = SortingMachine(board, cassette, gates_list, ir_pin)
-    
+    print("Sorting machine initialized successfully !!!")
     return sm
            
