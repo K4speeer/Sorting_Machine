56a58ea3c14e8fe71e077bdf25efcb45a5db0582
K4speeer <dri.taha24@gmail.com>
2023-08-29 10:36:22 +0300

    Updated __init__.py functions and imports

--- sorting_machine/__init__.py
+++ sorting_machine/__init__.py
@@ -1,9 +1,6 @@
 # Import Pymata4 library for serial communication 
 from pymata4 import pymata4
-import time
-import os
-import datetime
-from .sm_objects import SortingMachine, Cassette, Item, Gate
+from .sm_objects import SortingMachine, Cassette, Gate
 from picamera2 import Picamera2
 
 
@@ -14,26 +11,30 @@ def init_board():
     Returns:
         Pymata4(): Arduino board instance as Pymata4() object
     """
+    board = pymata4.Pymata4()
+    print(f"{board.get_firmware_version()}")
+    print(f"{board.get_protocol_version()}")
+    print(f"{board.get_pymata_version()}")
     print("Arduino board as Pymata4() instance initialized")
-    return pymata4.Pymata4()
+    return board
 
 def init_machine(board, ir_pin, step_pin, dir_pin, servo_configs):
     # Initialize the machine object and set pins to use.
 
     cassette = Cassette(board, step_pin, dir_pin)
-    print("Casseette instance initialized with parameters: "/
-          f"Step pin of driver = {step_pin}"/
+    print(f"Casseette instance initialized with parameters: "\
+          f"Step pin of driver = {step_pin}"\
           f"Direction pin of driver = {dir_pin}")
     gates_list = []
-    for key, value in servo_configs:
+    for key, value in servo_configs.items():
         pin = key
         close_pos = value[0]
         open_pos = value[1]
         pos = value[2]
         gate = Gate(board, pin, close_pos, open_pos, pos)
-        print(f"Servo connected to pin: {key} initialized successfully with parameters: "/
-              f"Close position: {close_pos}"/
-              f"Open position: {open_pos}"/
+        print(f"Servo connected to pin: {key} initialized successfully with parameters: "\
+              f"Close position: {close_pos}"\
+              f"Open position: {open_pos}"\
               f"Position depending on cassette origin: {pos}")
         gate.close()
         gates_list.append(gate)
